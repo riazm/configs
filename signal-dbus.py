c16992e4e908a63cf2a2109c5478f651a551824c
riaz moola <theongoingrealityshow@gmail.com>
2020-07-31 11:21:50 +0000

    change to signal-dbus.py

--- signal-dbus.py
+++ signal-dbus.py
@@ -1,7 +1,12 @@
 def msgRcv (timestamp, source, groupID, message, attachments):
+   
    contact = signal.getContactName(source)
    if (not contact):
-      contact = source
+      if (source):
+         contact = source
+      else:
+         contact = "Unknown"
+      
    if (groupID):
       print (contact + ", in group " + signal.getGroupName(groupID) + ": " + message)
    elif(attachments):
@@ -10,6 +15,14 @@ def msgRcv (timestamp, source, groupID, message, attachments):
       print (contact + ": " + message)
    return
 
+def recRcv(timestamp, source):
+   contact = signal.getContactName(source)
+   if (not contact):
+      if (source):
+         contact = source
+      else:
+         contact = "Unknown"
+   
 from pydbus import SystemBus
 from gi.repository import GLib
 
@@ -18,6 +31,7 @@ loop = GLib.MainLoop()
 
 signal = bus.get('org.asamk.Signal')
 signal.onMessageReceived = msgRcv
+signal.onReceiptReceived = recRcv
 #print(signal)
 #help(signal)
 loop.run()
